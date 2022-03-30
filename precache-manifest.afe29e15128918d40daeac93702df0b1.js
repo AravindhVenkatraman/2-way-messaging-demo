3119356caea4cb703aa081b4113f1a35de920466
Aravindh Prakash Venkatraman <avenkatraman@togetherwork.com>
2022-03-30 09:03:52 +0530

    Updates

---
+++ precache-manifest.afe29e15128918d40daeac93702df0b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b899d3aa30b55eef2b235690bc049635",
+    "url": "/2-way-messaging-demo/index.html"
+  },
+  {
+    "revision": "198f6bce14d602fa548f",
+    "url": "/2-way-messaging-demo/static/css/2.04cd50ce.chunk.css"
+  },
+  {
+    "revision": "47a7e03983df0fd3a5ac",
+    "url": "/2-way-messaging-demo/static/css/main.5f78491d.chunk.css"
+  },
+  {
+    "revision": "198f6bce14d602fa548f",
+    "url": "/2-way-messaging-demo/static/js/2.fada429e.chunk.js"
+  },
+  {
+    "revision": "989fda785c5dede04d809521fb0e8c5b",
+    "url": "/2-way-messaging-demo/static/js/2.fada429e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40517fc902a3ea33b71",
+    "url": "/2-way-messaging-demo/static/js/3.1f7b4ed3.chunk.js"
+  },
+  {
+    "revision": "42e96760e87007f22db9",
+    "url": "/2-way-messaging-demo/static/js/4.5067f556.chunk.js"
+  },
+  {
+    "revision": "f82ac294eeaf232deec6",
+    "url": "/2-way-messaging-demo/static/js/5.a12b7e29.chunk.js"
+  },
+  {
+    "revision": "47a7e03983df0fd3a5ac",
+    "url": "/2-way-messaging-demo/static/js/main.8ced70d3.chunk.js"
+  },
+  {
+    "revision": "03c2f387a35e3d9114d2",
+    "url": "/2-way-messaging-demo/static/js/runtime-main.f21be725.js"
+  },
+  {
+    "revision": "1499f61662a47060fa16058d04bd1ce7",
+    "url": "/2-way-messaging-demo/static/media/EmojiOneColor.1499f616.woff2"
+  },
+  {
+    "revision": "0daa6ab9bbf363b6f8064fdf1763045f",
+    "url": "/2-way-messaging-demo/static/media/NotoColorEmoji-flags.0daa6ab9.woff2"
+  },
+  {
+    "revision": "b3aba88d780e9eed234736fac30764f6",
+    "url": "/2-way-messaging-demo/static/media/str-chat__connection-error.b3aba88d.svg"
+  }
+]);
