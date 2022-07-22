cd32caddb0e6e080d92e4c9e01983d17daa42179
Aravindh Prakash Venkatraman <avenkatraman@togetherwork.com>
2022-07-22 16:10:15 +0530

    Minor changes in stream

--- src/components/App.js
+++ src/components/App.js
@@ -67,36 +67,21 @@ const App = () => {
 
   // feather.replace();
 
-  const controls = document.querySelector('.controls');
-  console.log(controls);
   const cameraOptions = document.querySelector('.video-options>select');
   const video = document.querySelector('video');
-  let constraints = {
-    video: {
-      width: {
-        min: 1280,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        min: 720,
-        ideal: 1080,
-        max: 1440
-      },
-      deviceId: {
-        exact: "",
-      }
-    }
-  };
+
   const cameraChange = (e) => {
     console.log(e);
     if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
       let updatedConstraints = { video: { deviceId: { exact: (e && e.target) ? e.target.value : "" } } }
-
+      if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+        updatedConstraints = { video: { groupId: { exact: (e && e.target) ? e.target.value : "" } } }
+      }
       console.log(updatedConstraints);
       startStream(updatedConstraints);
     }
   }
+
   document.getElementById("camera-select").addEventListener("change", cameraChange);
 
   const getCameraSelection = async () => {
@@ -104,8 +89,16 @@ const App = () => {
     let videoDevices = devices.filter(device => device.kind === 'videoinput');
     console.log(videoDevices);
     let options = ["<option value=''>Select camera</option>"];
-    let optionsArr = videoDevices.map(videoDevice => {
-      return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+    let optionsArr = videoDevices.map((videoDevice, index) => {
+      let label = "Camera-" + index + 1;
+      if (videoDevice.label.length !== 0) {
+        label = videoDevice.label;
+      }
+      let value = videoDevice.deviceId;
+      if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+        value = videoDevice.groupId;
+      }
+      return `<option value="${value}">${label}</option>`;
     });
     options = options.concat(optionsArr);
     console.log(options);
